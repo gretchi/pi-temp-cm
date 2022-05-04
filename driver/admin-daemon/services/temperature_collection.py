f187da3b0ad33d9947fbfade67b685841e0f26cb
gretchi <randozou@gmail.com>
2022-05-04 22:16:48 +0900

    Sync by Linux in gretel@gn-sv-11

--- driver/admin-daemon/services/temperature_collection.py
+++ driver/admin-daemon/services/temperature_collection.py
@@ -13,6 +13,17 @@ class TemperatureCollection(ServiceBase):
     def job(self):
         logging.info("Running!!")
 
+        mac = "C4:43:D5:0D:4D:F4"
+
+        sensor = Sensor(mac)
+        sensor.scan(timeout=20)
+
+        temp = sensor.get("Temperature")
+        humidity = sensor.get("Humidity")
+        battery = sensor.get("BatteryVoltage")
+
+        logging.info(f"mac: {mac}, temp: {temp}, humidity: {humidity}")
+
         time.sleep(10)
 
         return
