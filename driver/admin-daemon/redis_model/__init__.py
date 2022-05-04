a10a11100c19d7d065e7f6483af3718b85dcc63d
gretchi <randozou@gmail.com>
2022-05-05 03:20:54 +0900

    Sync by Linux in gretel@gn-sv-11

--- driver/admin-daemon/redis_model/__init__.py
+++ driver/admin-daemon/redis_model/__init__.py
@@ -7,5 +7,5 @@ class RedisModel(object):
         self.redis = redis.Redis(host='localhost', port=6379, db=0)
 
     def set_sensor(self, location, mac):
-        self.redis.hset("sensor", "location", location)
-        self.redis.hset("sensor", "mac", mac)
+        self.redis.hset(mac, "location", location)
+        self.redis.hset(mac, "mac", mac)
