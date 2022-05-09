76a75a6781bad5fb89da1105a1dc8acfa3e5dbd7
gretchi <randozou@gmail.com>
2022-05-09 20:52:00 +0900

    Sync by Linux in gretel@gn-sv-11

--- driver/admin-daemon/redis_model/__init__.py
+++ driver/admin-daemon/redis_model/__init__.py
@@ -1,19 +1,36 @@
 
+import json
 import redis
 import helper
 
+SENSORS_KEY = "sensors"
+PLUGS_KEY = "plugs"
 
 class RedisModel(object):
     def __init__(self):
-        self.redis = redis.Redis(host='localhost', port=6379, db=0)
+        self.conn = redis.Redis(host='localhost', port=6379, db=0)
 
     def set_sensor(self, location, mac):
         mac = helper.net.insert_delimiter(mac, "-")
 
-        self.redis.hset(mac, "location", location)
-        self.redis.hset(mac, "mac", mac)
 
+        self.conn.set(SENSORS_KEY, )
 
-    def comvert_from_mac(self, mac):
-
+    def get_sensors(self):
         pass
+
+
+    def jget(self, key):
+        j = self.conn.get(key)
+        data = json.loads(j)
+
+        return data
+
+    def jhset(self, key, data):
+        db_data = self.jget(key)
+        j = json.dumps(db_data)
+        db_data
+
+        self.conn.set(key, j)
+
+        return
